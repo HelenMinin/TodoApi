926a0924b1aabd9a0bd2081c8e34a67775aff858
HelenMinin <helenminis@gmail.com>
2022-06-14 21:10:42 -0300

    Criando primeiras tabelas no banco de dados

---
+++ Models/Todo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Concurrent;
+
+namespace ToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
